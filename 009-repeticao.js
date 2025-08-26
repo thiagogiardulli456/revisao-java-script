dbc5ca3fcb37772a5875489cd672d444b7698170
Thiago Giardulli <thiagomsgiardulli@gmail.com>
2025-08-25 21:40:13 -0300

    Create 009-repeticao.js

---
+++ 009-repeticao.js
@@ -0,0 +1,12 @@
+    let nInicio = 10;
+    let nFim = 20;
+    let repeticoes = 0;
+
+    // Repetição com teste no Início
+    while (nInicio <= nFim) {
+        console.log(nInicio);
+        nInicio++;
+        repeticoes++;
+    }
+
+    console.log(`O código foi repetido ${repeticoes} vezes!`);
