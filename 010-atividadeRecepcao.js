3fd33bc27b5e66e70fb4ff3ada9ace246ef40160
Thiago Giardulli <thiagomsgiardulli@gmail.com>
2025-08-29 22:49:11 -0300

    10 atividade

---
+++ 010-atividadeRecepcao.js
@@ -0,0 +1,4 @@
+/*
+Crie um programa que receba dois numero e apresente a contagem de primeiro número seja maior a contagem deve ser decrescente.
+Trate também possíveis erros nos valores digitados
+*/
